3777a5a5ad99ecdfa4b08750ab2d97df530541de
nooruto <noor@innov8tif.com>
2025-11-09 20:34:11 +0800

    feat: #5 - save airtable data to use

--- src/main/java/com/techsisters/gatherly/service/UserService.java
+++ src/main/java/com/techsisters/gatherly/service/UserService.java
@@ -2,6 +2,9 @@ package com.techsisters.gatherly.service;
 
 import org.springframework.stereotype.Service;
 
+import com.techsisters.gatherly.entity.User;
+import com.techsisters.gatherly.integration.airtable.AirtableService;
+import com.techsisters.gatherly.integration.airtable.response.Record;
 import com.techsisters.gatherly.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +16,45 @@ import lombok.extern.slf4j.Slf4j;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AirtableService airtableService;
 
-    
+    /*
+     * Check if user is a registered Techsisters member via Airtable
+     * if valid generate 6-digit OTP and send to email
+     */
+    public Integer generateOTP(String email) throws Exception {
+        log.info("Checking if user with email {} is a Techsisters member", email);
 
+        Integer otp = null;
+
+        Record userRecord = airtableService.findByEmail(email);
+        if (userRecord != null) {
+            log.info("User with email {} is a Techsisters member", email);
+
+            // create/update user in DB
+            User user = findByEmail(email);
+            if (user == null) {
+                user = new User();
+            }
+            user = copyAirtableData(user, userRecord);
+
+        } else {
+            log.info("User with email {} is not a Techsisters member", email);
+            throw new IllegalArgumentException("User " + email + " is not a Techsisters member");
+        }
+
+        return otp;
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+
+    public User copyAirtableData(User user, Record record) {
+        user.setName(record.getFields().getName());
+        user.setEmail(record.getFields().getEmail());
+        user.setCountry(record.getFields().getCountry());
+
+        return userRepository.save(user);
+    }
 }
