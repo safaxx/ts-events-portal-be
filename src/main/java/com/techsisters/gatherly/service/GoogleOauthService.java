e07af63b37caec248c15b173e521993ae19a685c
nooruto <noor@innov8tif.com>
2025-11-24 17:10:32 +0800

    Temporarily remove oauth codes

--- src/main/java/com/techsisters/gatherly/service/GoogleOauthService.java
+++ src/main/java/com/techsisters/gatherly/service/GoogleOauthService.java
@@ -11,10 +11,8 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@RequiredArgsConstructor
 @Slf4j
 @Service
 public class GoogleOauthService {
@@ -28,23 +26,6 @@ public class GoogleOauthService {
     private static final java.util.Collection<String> SCOPES = Collections
             .singletonList("https://www.googleapis.com/auth/calendar.events");
 
-    private final GoogleAuthorizationCodeFlow flow;
-
-    public GoogleOauthService() throws IOException {
-        flow = new GoogleAuthorizationCodeFlow.Builder(
-                new NetHttpTransport(), // HTTP Transport layer
-                GsonFactory.getDefaultInstance(), // JSON parser
-                clientId,
-                clientSecret,
-                SCOPES)
-                .build();
-    }
-
-    public GoogleTokenResponse exchangeCodeForTokens(String authorizationCode) throws IOException {
-        GoogleTokenResponse response = flow.newTokenRequest(authorizationCode)
-                .execute();
-
-        return response;
-    }
+   
 
 }
